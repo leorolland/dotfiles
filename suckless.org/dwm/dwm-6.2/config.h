ad8c1e80d9fa594a4b2f2f59e4d6276aaaaa508f
Léo Rolland <rolland.leo@orange.fr>
2020-12-01 14:25:11 +0000

    updated dwm theme

--- suckless.org/dwm/dwm-6.2/config.h
+++ suckless.org/dwm/dwm-6.2/config.h
@@ -15,7 +15,8 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#57B54A";
+// static const char col_cyan[]        = "#57B54A"; // green
+static const char col_cyan[]        = "#e8003a"; 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
